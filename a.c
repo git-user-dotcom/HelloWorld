a1bdcb8ef1de621741142488af99bfb72b0b36c2
amkansara <amkansara98@gmail.com>
2018-08-30 23:30:45 +0530

    Update a.c

--- a.c
+++ a.c
@@ -1,11 +1,12 @@
 # HelloWorld
 
+
+//C File
 #include<stdio.h>
 void main(){
 printf("Hello World");
 }
-
-
+//Java File
 class HelloWorld{
   public static void main(String args[]){
     System.out.println("HelloWorld");
